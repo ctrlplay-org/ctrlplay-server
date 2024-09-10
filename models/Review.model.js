90be06ff38d30918dfc8ecc0faa34d665d0e1029
Mael Micout <micoutmael3@gmail.com>
2024-09-10 16:48:26 +0200

    Modified the model of reviews and routes

--- models/Review.model.js
+++ models/Review.model.js
@@ -2,35 +2,14 @@ const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const reviewSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: [true, "Review title is required."],
-      trim: true,
-    },
-    date: {
-      type: Date,
-      required: [true, "Date created is required."],
-    },
-    description: String,
-    game: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Game",
-        required: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    rating: Number,
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+const reviewSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  rating: { type: Number, required: true },
+  game: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 
+  date: { type: Date, default: Date.now },
+});
 
 const Review = model("Review", reviewSchema);
 
--- routes/Review.routes.js
+++ routes/Review.routes.js
@@ -10,18 +10,37 @@ const Review = require('../models/Review.model');
 // POST a new review
 
 router.post("/reviews", isAuthenticated, (req, res, next) => {
-    Review.create(req.body)
-    .then(review => {
-        return Game.findByIdAndUpdate(
-            req.body.game,
-            { $push: { reviews: review._id } },
-            { new: true, useFindAndModify: false }
-        ).then(() => {
-            res.status(201).json(review);
+    
+    const author = req.payload?._id;
+
+    
+    if (!author) {
+        return res.status(400).json({ message: 'Author not authenticated' });
+    }
+
+    
+    const { title, description, rating, game } = req.body;
+
+    
+    Review.create({ title, description, rating, game, author })
+        .then(review => {
+            return Game.findByIdAndUpdate(
+                req.body.game,
+                { $push: { reviews: review._id } },
+                { new: true, useFindAndModify: false }
+            ).then(() => {
+                res.status(201).json(review);
+            });
+        })
+        .catch(error => {
+            console.error(error); 
+            next(error);
         });
-    })
-        .catch(error => next(error));
 });
+
+
+
+
 
 // GET all reviews
 
@@ -64,17 +83,34 @@ router.put("/reviews/:reviewId", isAuthenticated, (req, res, next) => {
 
 // DELETE review for specific review ID
 
+// DELETE review for specific review ID
 router.delete("/reviews/:reviewId", isAuthenticated, (req, res, next) => {
     const { reviewId } = req.params;
-    Review.findByIdAndDelete(reviewId)
-        .then(() => {
-            return Game.findOneAndUpdate(
-                { reviews: reviewId },
-                { $pull: { reviews: reviewId } }
-            );
+    const userId = req.payload._id;
+
+    // Find the review by ID
+    Review.findById(reviewId)
+        .then(review => {
+            if (!review) {
+                return res.status(404).json({ message: "Review not found" });
+            }
+            // Check if the current user is the author of the review
+            if (review.author.toString() !== userId.toString()) {
+                return res.status(403).json({ message: "You are not authorized to delete this review" });
+            }
+            // Proceed to delete the review if the user is the author
+            return Review.findByIdAndDelete(reviewId)
+                .then(() => {
+                    return Game.findOneAndUpdate(
+                        { reviews: reviewId },
+                        { $pull: { reviews: reviewId } }
+                    );
+                })
+                .then(() => res.status(204).send())
+                .catch(error => next(error));
         })
-        .then(() => res.status(204).send())
         .catch(error => next(error));
 });
+
 
 module.exports = router;
