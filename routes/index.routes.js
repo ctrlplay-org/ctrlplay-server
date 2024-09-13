42cc741b1ceb00e1131afb8e89faa210ad4d49d1
Mael Micout <micoutmael3@gmail.com>
2024-09-13 10:37:50 +0200

    add health in index.routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Game = require("../models/Game.model")
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
 router.get('/health', (req, res) => {
+
+  Game.find().then().catch();
+
   res.status(200).json({
       status: "OK",
       uptime: process.uptime(),
